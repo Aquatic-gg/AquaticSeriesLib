c4089bab5ebeef7d8476edaf48f7564747025be9
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-09 00:50:36 +0200

    Add sendTitle function to AquaticString extension
    This new function allows sending a title and subtitle to a player with fade in, stay, and fade out durations. It leverages the existing titleAdapter within AquaticSeriesLib to achieve this functionality.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/StringExt.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/StringExt.kt
@@ -1,6 +1,7 @@
 package gg.aquatic.aquaticseries.lib
 
 import gg.aquatic.aquaticseries.lib.adapt.AquaticString
+import org.bukkit.entity.Player
 
 fun String.toAquatic(): AquaticString {
     return AquaticSeriesLib.INSTANCE.adapter.adaptString(this)
@@ -8,4 +9,21 @@ fun String.toAquatic(): AquaticString {
 
 fun Collection<String>.toAquatic(): List<AquaticString> {
     return map { it.toAquatic() }
+}
+
+fun AquaticString.sendTitle(
+    player: Player,
+    title: AquaticString,
+    subtitle: AquaticString,
+    fadeIn: Int,
+    stay: Int,
+    fadeOut: Int
+) {
+    AquaticSeriesLib.INSTANCE.adapter.titleAdapter.send(
+        player = player,
+        title = title,
+        subtitle = subtitle,
+        fadeIn = fadeIn,
+        stay = stay,
+        fadeOut = fadeOut)
 }
