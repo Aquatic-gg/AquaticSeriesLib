2663e236b5f806ef3efaa54bca98dae0bed0fa84
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-12 14:04:26 +0200

    Add tab completion to ICommand interface
    Introduced a new method `tabComplete` to the `ICommand` interface to provide suggestions based on the command input. This enhances the command handling mechanism, offering users completion hints as they type.

--- core/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/ICommand.kt
+++ core/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/ICommand.kt
@@ -5,5 +5,6 @@ import org.bukkit.command.CommandSender
 interface ICommand {
 
     fun run(sender: CommandSender, args: Array<out String>)
+    fun tabComplete(sender: CommandSender, args: Array<out String>): List<String>
 
 }
