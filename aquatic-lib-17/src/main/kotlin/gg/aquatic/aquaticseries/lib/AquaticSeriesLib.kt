3c16b6420551b6c01fe89545c3465bde91291863
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-14 16:54:13 +0200

    Add NMS adapter support for Minecraft 1.17.1
    Integrated NMS adapter selection with a specific implementation for Minecraft version 1.17.1. This allows the library to correctly initialize and use the appropriate NMS features for this version of the game.

--- aquatic-lib-17/src/main/kotlin/gg/aquatic/aquaticseries/lib/AquaticSeriesLib.kt
+++ aquatic-lib-17/src/main/kotlin/gg/aquatic/aquaticseries/lib/AquaticSeriesLib.kt
@@ -3,6 +3,7 @@ package gg.aquatic.aquaticseries.lib
 import gg.aquatic.aquaticseries.lib.feature.Features
 import gg.aquatic.aquaticseries.lib.feature.IFeature
 import gg.aquatic.aquaticseries.lib.nms.NMSAdapter
+import gg.aquatic.aquaticseries.nms.NMS_1_17_1
 import org.bukkit.plugin.java.JavaPlugin
 
 class AquaticSeriesLib private constructor(
@@ -24,8 +25,58 @@ class AquaticSeriesLib private constructor(
         fun init(plugin: JavaPlugin, features: Collection<IFeature>): AquaticSeriesLib {
             val instance = _INSTANCE
             if (instance != null) return instance
-            _INSTANCE = AquaticSeriesLib(plugin, null, HashMap(features.associateBy { it.type }))
+            val adapter = chooseNMSAdapter(plugin)
+            _INSTANCE = AquaticSeriesLib(plugin, adapter, HashMap(features.associateBy { it.type }))
             return _INSTANCE!!
+        }
+
+        private fun chooseNMSAdapter(plugin: JavaPlugin): NMSAdapter? {
+            when (plugin.server.bukkitVersion) {
+                "1.16.5-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.17.1-R0.1-SNAPSHOT" -> {
+                    return NMS_1_17_1()
+                }
+
+                "1.18.2-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.19.2-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.19.3-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.19.4-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.20.1-R0.1-SNAPSHOT", "1.20-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.20.2-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.20.4-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.20.5-R0.1-SNAPSHOT" -> {
+
+                }
+
+                "1.21-R0.1-SNAPSHOT" -> {
+
+                }
+            }
+            return null
         }
     }
 
