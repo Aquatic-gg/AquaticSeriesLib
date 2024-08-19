07ffcf257aca8c5e02288bc56aff8bcb44f86391
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-19 14:52:06 +0200

    Update project version to 1.0.2.5.
    Bump project version from 1.0.2.4 to 1.0.2.5 in the build script. This update ensures consistency with the latest changes and prepares the project for the new release cycle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.2.4"
+val projectVersion = "1.0.2.5"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
