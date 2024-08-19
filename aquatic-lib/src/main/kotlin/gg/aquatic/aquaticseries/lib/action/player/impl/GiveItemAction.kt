cafecab4ecc8bb7861711fd3df4b816467c9f433
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-19 14:47:58 +0200

    Add GiveItemAction to handle item distribution
    Introduced a new class `GiveItemAction` to facilitate giving custom items to players. This class extends `AbstractPlayerAction` and implements required methods to manage item distribution and argument handling. This addition enhances item management capabilities within the player action framework.

---
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/action/player/impl/GiveItemAction.kt
@@ -0,0 +1,23 @@
+package gg.aquatic.aquaticseries.lib.action.player.impl
+
+import gg.aquatic.aquaticseries.lib.action.player.AbstractPlayerAction
+import gg.aquatic.aquaticseries.lib.item.CustomItem
+import gg.aquatic.aquaticseries.lib.util.argument.AquaticObjectArgument
+import gg.aquatic.aquaticseries.lib.util.argument.impl.ItemObjectArgument
+import gg.aquatic.aquaticseries.lib.util.placeholder.Placeholders
+import gg.aquatic.aquaticseries.lib.util.toCustomItem
+import org.bukkit.Material
+import org.bukkit.entity.Player
+
+class GiveItemAction: AbstractPlayerAction() {
+    override fun run(binder: Player, args: Map<String, Any?>, placeholders: Placeholders) {
+        val customItem = args["item"] as? CustomItem ?: return
+        customItem.giveItem(binder)
+    }
+
+    override fun arguments(): List<AquaticObjectArgument<*>> {
+        return listOf(
+            ItemObjectArgument("item", Material.STONE.toCustomItem(), true)
+        )
+    }
+}
