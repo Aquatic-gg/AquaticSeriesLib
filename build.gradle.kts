a02747e773f056c8d8389550a77f981152bbb9fa
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-19 16:07:12 +0200

    Update project version to 1.0.2.6
    This change increments the project's version from 1.0.2.5 to 1.0.2.6 in `build.gradle.kts`. It prepares the project for the next release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.2.5"
+val projectVersion = "1.0.2.6"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
