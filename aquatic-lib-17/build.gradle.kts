1907091c8977024b3ec7fa6ded5aafc302c447e7
MrLarkyy_ <studiotadikl@gmail.com>
2024-08-20 20:33:22 +0200

    Update NMS version to 1.0.14
    Updated the NMS version in build files from 1.0.13 to 1.0.14 for aquatic-lib-17 and aquatic-lib-21. This ensures compatibility with the latest NMS updates and provides the latest features and fixes.

--- aquatic-lib-17/build.gradle.kts
+++ aquatic-lib-17/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 val maven_username: String by rootProject.extra
 val maven_password: String by rootProject.extra
 
-val nmsVersion = "1.0.13"
+val nmsVersion = "1.0.14"
 
 group = "gg.aquatic.aquaticseries"
 
