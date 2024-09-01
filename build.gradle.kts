3167b8ff47b9f798928e40c21d52e09f888201dd
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-01 22:05:37 +0200

    Update project version to 1.0.4.0
    This change updates the project version from 1.0.3.105 to 1.0.4.0 in the build.gradle.kts file. It ensures that the project versioning remains consistent with recent changes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.3.105"
+val projectVersion = "1.0.4.0"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
