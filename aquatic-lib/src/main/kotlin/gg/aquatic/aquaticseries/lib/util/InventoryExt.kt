ba5a0d117d94f16d6afdb08bf4887536216af0f2
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-05 12:10:30 +0200

    Refactor Inventory.addItem method
    Rename addItem to addOrDropItem and remove dropIfFull parameter to simplify the function logic. The method now always drops items if the inventory is full.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/InventoryExt.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/InventoryExt.kt
@@ -4,9 +4,8 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
-fun Inventory.addItem(dropIfFull: Boolean, vararg itemStack: ItemStack) {
+fun Inventory.addOrDropItem(vararg itemStack: ItemStack) {
     val map = this.addItem(*itemStack)
-    if (!dropIfFull) return
     if (this.holder is Player) {
         val location = (this.holder as Player).location
         for (value in map.values) {
