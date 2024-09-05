666394f2d2a44578b1ab6867fa029091026169e9
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-05 12:10:01 +0200

    Add early return if dropIfFull is false
    This change adds an early return in the addItem function when the dropIfFull parameter is false. It prevents dropping items if the inventory is full and the dropIfFull option is not enabled. This improves performance and avoids unnecessary item drops.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/InventoryExt.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/InventoryExt.kt
@@ -6,6 +6,7 @@ import org.bukkit.inventory.ItemStack
 
 fun Inventory.addItem(dropIfFull: Boolean, vararg itemStack: ItemStack) {
     val map = this.addItem(*itemStack)
+    if (!dropIfFull) return
     if (this.holder is Player) {
         val location = (this.holder as Player).location
         for (value in map.values) {
