8f2171cb723e6972a7a3d73b7e441d912f75e44d
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-05 02:04:18 +0200

    Add chat number input validation
    Allow chat input to validate numbers in a new ChatNumber class. Enhance existing Chat class to accept validators for better input handling. These changes improve user input processing and validation consistency.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/editor/type/ChatNumber.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/editor/type/ChatNumber.kt
@@ -5,21 +5,29 @@ import gg.aquatic.aquaticseries.lib.editor.editable.Editable
 import gg.aquatic.aquaticseries.lib.input.type.chat.ChatInputHandler
 import gg.aquatic.aquaticseries.lib.input.type.chat.TextInputResponse
 import gg.aquatic.aquaticseries.lib.input.type.chat.TextValidator
+import gg.aquatic.aquaticseries.lib.toAquatic
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.concurrent.CompletableFuture
+import java.util.function.Function
 
-class Chat(
+class ChatNumber(
     val name: String,
     val item: ItemStack,
     vararg val validators: TextValidator
-): Edit<String> {
-    override fun handle(editable: Editable<String>, player: Player): CompletableFuture<String> {
-        return ChatInputHandler.create(player, *validators).thenApply {
+): Edit<Number> {
+    override fun handle(editable: Editable<Number>, player: Player): CompletableFuture<Number> {
+
+        return ChatInputHandler.create(player, *validators, TextValidator(
+            { str ->
+                str.toDoubleOrNull() != null
+            },
+            "Invalid number format!".toAquatic()
+        )).thenApply {
             if (it.response != TextInputResponse.Response.SUCCESS) {
                 editable.value
             } else {
-                it.message
+                it.message!!.toDouble()
             }
         }
     }
