caf613e81c2e01484ce9e12781880efcbb3c1e9d
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-05 12:09:39 +0200

    Add Inventory.addItem extension function
    Introduced an extension function to add items to an inventory with an option to drop items if full. Handles the case where the holder is a player and drops the overflow items at the player's location.

---
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/InventoryExt.kt
@@ -0,0 +1,16 @@
+package gg.aquatic.aquaticseries.lib.util
+
+import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+fun Inventory.addItem(dropIfFull: Boolean, vararg itemStack: ItemStack) {
+    val map = this.addItem(*itemStack)
+    if (this.holder is Player) {
+        val location = (this.holder as Player).location
+        for (value in map.values) {
+            location.world?.dropItem(location, value)
+        }
+    }
+
+}
