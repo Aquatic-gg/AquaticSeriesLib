594ed432e92850df1206a92dd570d58f94457b3c
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-07 02:25:07 +0200

    Update project version and reorder color patterns
    Updated the project version from 1.0.4.36 to 1.0.4.38 in build.gradle.kts. Additionally, reordered the pattern list in ColorUtils.kt for better readability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.4.36"
+val projectVersion = "1.0.4.38"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
