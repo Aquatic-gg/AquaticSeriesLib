313a7b3ddc9c77425d9add4957c0a4c59b145d64
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-10 16:33:05 +0200

    Update project version to 1.0.4.41
    Incremented the project version from 1.0.4.40 to 1.0.4.41 in the build script. This update ensures the project reflects the latest changes and improvements.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.4.40"
+val projectVersion = "1.0.4.41"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
