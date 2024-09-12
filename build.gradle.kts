f59aa1b2fbba4d8ea2e6cfd617266e5f9fb85058
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-12 22:16:46 +0200

    Update project version to 1.0.4.44
    This change increments the project version from 1.0.4.43 to 1.0.4.44 in the build.gradle.kts file. It ensures version consistency for the upcoming release and helps in tracking changes more effectively.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.4.43"
+val projectVersion = "1.0.4.44"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
