65231cc0df6f60e71b6f02d3f0c92ce3379c75b3
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-12 22:16:21 +0200

    Add List extension functions for mapping to pairs
    Introduce mapPair and mapPairNotNull extensions to List. These functions facilitate creating MutableMaps from lists by applying a processor function.

---
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/ListExt.kt
@@ -0,0 +1,17 @@
+package gg.aquatic.aquaticseries.lib.util
+
+inline fun <T,A,B> List<T>.mapPair(processor: (T) -> Pair<A,B>): MutableMap<A,B> {
+    val map = mutableMapOf<A,B>()
+    this.forEach {
+        map += processor(it)
+    }
+    return map
+}
+
+inline fun <T,A,B> List<T>.mapPairNotNull(processor: (T) -> Pair<A,B>?): MutableMap<A,B> {
+    val map = mutableMapOf<A,B>()
+    for (it in this) {
+        map += processor(it) ?: continue
+    }
+    return map
+}
