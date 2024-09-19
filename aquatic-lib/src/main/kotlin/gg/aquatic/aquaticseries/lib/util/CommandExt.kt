032dc15499b8ff86b45813ecc503d8f0b45dce9f
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-19 11:48:31 +0200

    Revamp command registration and unregistration
    Simplify the command registration process by directly accessing the command map and clearing commands before registering. Additionally, introduce a new method to unregister commands, ensuring that both commands and their aliases are properly removed from the map.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/CommandExt.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/CommandExt.kt
@@ -1,19 +1,33 @@
 package gg.aquatic.aquaticseries.lib.util
 
 import org.bukkit.Bukkit
+import org.bukkit.Server
 import org.bukkit.command.Command
 import org.bukkit.command.CommandMap
 import java.lang.reflect.Field
 
 fun Command.register(namespace: String) {
-    try {
-        val bukkitCommandMap: Field = Bukkit.getServer().javaClass.getDeclaredField("commandMap")
+    Bukkit.getServer().commandMap().clearCommands()
+    Bukkit.getServer().commandMap().register(namespace, this)
+}
 
-        bukkitCommandMap.setAccessible(true)
-        val commandMap: CommandMap = bukkitCommandMap.get(Bukkit.getServer()) as CommandMap
-
-        commandMap.register(namespace, this)
-    } catch (e: Exception) {
-        e.printStackTrace()
+fun Command.unregister() {
+    val commandMap = Bukkit.getServer().commandMap()
+    val knownCommands = commandMap.knownCommands()
+    knownCommands.remove(name)
+    for (alias in this.aliases) {
+        if (knownCommands.containsKey(alias) && knownCommands[alias]!!.name.contains(this.name))
+            knownCommands.remove(alias)
     }
+}
+
+private val bukkitCommandMapField: Field = Bukkit.getServer().javaClass.getDeclaredField("commandMap").apply { isAccessible = true }
+private val knownCommandsField = CommandMap::class.java.getDeclaredField("knownCommands").apply { isAccessible = true }
+
+fun Server.commandMap(): CommandMap {
+    return bukkitCommandMapField.get(this) as CommandMap
+}
+
+fun CommandMap.knownCommands(): HashMap<String, Command> {
+    return knownCommandsField.get(this) as HashMap<String, Command>
 }
