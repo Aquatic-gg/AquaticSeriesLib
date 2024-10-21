905d9e845266883262f26d5cc929d217c3e85c42
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-21 23:52:14 +0200

    Upgrade project version and add new collection extensions
    Bump the project version to 1.0.29. Introduce new extension functions in CollectionExt.kt for handling ConfiguredRequirement and ConfiguredAction collections.

--- aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/CollectionExt.kt
+++ aquatic-lib/src/main/kotlin/gg/aquatic/aquaticseries/lib/util/CollectionExt.kt
@@ -1,11 +1,27 @@
 package gg.aquatic.aquaticseries.lib.util
 
+import gg.aquatic.aquaticseries.lib.action.ConfiguredAction
+import gg.aquatic.aquaticseries.lib.requirement.ConfiguredRequirement
+import java.util.function.BiFunction
+
 inline fun <T,A,B> Collection<T>.mapPair(processor: (T) -> Pair<A,B>): MutableMap<A,B> {
     val map = mutableMapOf<A,B>()
     this.forEach {
         map += processor(it)
     }
     return map
+}
+
+inline fun <reified T: Any> Collection<ConfiguredRequirement<T>>.checkRequirements(binder: T): Boolean {
+    for (configuredRequirement in this) {
+        if (!configuredRequirement.check(binder)) return false
+    }
+    return true
+}
+inline fun <reified T: Any> Collection<ConfiguredAction<T>>.executeActions(binder: T, textUpdater: BiFunction<T,String,String>) {
+    for (configuredAction in this) {
+        configuredAction.run(binder, textUpdater)
+    }
 }
 
 inline fun <T,A,B> Collection<T>.mapPairNotNull(processor: (T) -> Pair<A,B>?): MutableMap<A,B> {
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.28"
+val projectVersion = "1.0.29"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
