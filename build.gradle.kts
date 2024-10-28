73b896e94755287a1b327253a7b40f05f51dfe7f
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-29 00:00:11 +0100

    Update project and NMS versions
    Incremented the project version from 1.0.33 to 1.0.34 in the root build script. Additionally, updated the NMS version from 1.0.34 to 1.0.35 in the aquatic-lib module's build script for consistency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "2.0.0"
 }
 
-val projectVersion = "1.0.33"
+val projectVersion = "1.0.34"
 
 group = "gg.aquatic.aquaticseries"
 version = projectVersion
